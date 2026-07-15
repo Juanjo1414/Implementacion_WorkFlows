31eb412e0cd093b9c0a4c638602910528522dbed
Juan Jose Jaramillo <juan.jaramillo14@eia.edu.co>
2026-07-15 15:09:07 -0500

    feat: Se crean los test de prueba

---
+++ calc/calc.py
@@ -0,0 +1,11 @@
+from typing import Union
+
+Number = Union[int, float]
+
+def add(a: Number, b: Number) -> Number:
+    """Add two numbers."""
+    return a + b
+
+def multiply(a: Number, b: Number) -> Number:
+    """Multiply two numbers."""
+    return a * b
